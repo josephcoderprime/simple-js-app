6fe584372078527921f72fe98c5cf1ce191ae843
josephcoderprime <77362650+josephcoderprime@users.noreply.github.com>
2021-02-12 19:37:17 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
 alert ('Hello world');
+let favoriteFood = 'pasta';
+document.write(favoriteFood);
+favoriteFood = 'pizza';
+document.write(favoriteFood);
+favoriteFood = 'rice';
+document.write(favoriteFood);
