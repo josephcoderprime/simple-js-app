47ce8c5ac836107fff14a66e413c47237c3c40aa
josephcoderprime <77362650+josephcoderprime@users.noreply.github.com>
2021-02-13 18:36:57 +0000

    Update scripts.js
    I have modified the code from the values not being in arrays with at least 3 objects.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,27 @@
-let pokemonList =
-[name: 'Bulbasaur',
-height: 7,
-catchRate: 11.9%, 
-type:['grass','poison'];
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    name: 'Lugia',
+    name: 'Latios'
+  },
+  {
+    height: 7,
+    height: 5,
+    height: 2
+  },
+  {
+    weight:'6.9 kg',
+    weight:'216 kg',
+    weight:'60 kg'
+  },
+  {
+    catchRate: 11.9%,
+    catchRate: 0%,
+    catchRate: 0%
+  },
+  {
+    type:['grass','poison'],
+    type:['psychic','flying'],
+    type:['psychic','dragon']
+  },
+]
