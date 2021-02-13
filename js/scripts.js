5e6ffef75ce0ff7b779682a80f45490a6b3d92b9
josephcoderprime <77362650+josephcoderprime@users.noreply.github.com>
2021-02-13 16:20:58 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-let pokemonList = {name: 'Bulbasaur', height: '0.7m', catchRate: '11.9%', type:['grass','poison']};
+let pokemonList =
+[name: 'Bulbasaur',
+height: 7,
+catchRate: 11.9%, 
+type:['grass','poison'];
