7cb26b4f2f8532839b0097c40ed985b728023007
josephcoderprime <77362650+josephcoderprime@users.noreply.github.com>
2021-02-14 19:50:01 +0000

    Update scripts.js
    Modified objects with appropriate properties and values.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,32 @@
+/**
+*objects
+* - a special variable that can hold multple values
+* obhjects consist of properties and values
+*/
+
+//Variable pokemonList
 let pokemonList = [
-  {
+  { // arrays with 3 objects containing 3 keys values.
     name: 'Bulbasaur',
-    name: 'Lugia',
-    name: 'Latios'
-  },
-  {
     height: 7,
-    height: 5,
-    height: 2
-  },
-  {
     weight:'6.9 kg',
+    abilities: 'Overgrow',
+    type:['grass','poison']
+    },
+  {
+    name: 'Lugia',
+    height: 5,
     weight:'216 kg',
-    weight:'60 kg'
-  },
+    abilities: 'Pressure',
+    type:['psychic','flying']
+    },
   {
-    catchRate: 11.9%,
-    catchRate: 0%,
-    catchRate: 0%
-  },
-  {
-    type:['grass','poison'],
-    type:['psychic','flying'],
+    name: 'Latios',
+    height: 2,
+    weight:'60 kg',
+    abilities: 'Levitate',
     type:['psychic','dragon']
   },
-]
+];
+//accessing name property
+console.log(pokemonList);
