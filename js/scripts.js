44f1109c3fce5af619b57c5f350f39ebcdbbe65d
josephcoderprime <77362650+josephcoderprime@users.noreply.github.com>
2021-02-27 09:26:21 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@
 */
 
 //Variable pokemonList
-let pokemonList = [
+let poke_mon = [
   { // arrays with 3 objects containing 3 keys values.
     name: 'Bulbasaur',
     height: 7,
@@ -32,7 +32,7 @@ let pokemonList = [
 /**FOR EACH LOOP: In each iteration the variable name
 will hold the value of an element inside the ArrayList/Array,
 starting from the first element.
-*/
+
 
 pokemonList.forEach(function(pokemonList){
   document.write(pokemonList.name + "<br>")
@@ -41,6 +41,7 @@ pokemonList.forEach(function(pokemonList){
   document.write(pokemonList.abilities + "<br>")
   document.write(pokemonList.type + "<br><br>")
 });
+**/
 
 /**Basic Function
 function hello(){
@@ -51,19 +52,50 @@ hello()
 
 //IIFE - Immediately Invoked Function Expression pokemonRepository
 let pokemonRepository = (function () {
-  
+
   function add(pokemon) {
-    pokemonList.push(pokemon);
+    poke_mon.push(pokemon);
   }
   function getAll() {
-    return pokemonList;
+    return poke_mon;
   }
+
+ function showDetails (poke_mon){
+   console.log(poke_mon.name);
+ }
+ /**DOM = application programming interface to interact with web pages
+ add content
+ delete content
+ change content**/
+  function addListItem(poke_mon){
+    let pokemonList = document.querySelector(".pokemonList"); /*Created a varialbe | querySelector() returns the first element
+    within the document the specified selector, or group of selectors. If no matches are found, null is returned.*/
+    let listpokemon = document.createElement("li"); /*The document.createElement() accepts an HTML tag name and returns a new Node with the Element type.*/
+    let button = document.createElement("button");
+    let button_1 = document.createElement("button_1");
+    button.innerText = poke_mon.name;
+    button.classList.add("button-class");
+    button.classList.add("button_1");
+    listpokemon.appendChild(button); /*The appendChild() method appends a node as the last child of a node.*/
+    pokemonList.appendChild(listpokemon);
+    listpokemon.appendChild(button_1);
+    button.addEventListener('click',function() {
+    showDetails(poke_mon);
+  });
+}
 
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails
   };
 })();
 
-pokemonRepository.add({name: 'Blastoise', height: 1.6, type:['Water'] });
+
+pokemonRepository.add({name: 'Blastoise', height: 1.6, type:['Water']}
+);
 console.log(pokemonRepository.getAll());
+pokemonRepository.getAll().forEach(function(poke_mon) {
+pokemonRepository.addListItem(poke_mon);
+});
