06931edcc58ceb3df463fed8f0d871628f7759d9
josephcoderprime <77362650+josephcoderprime@users.noreply.github.com>
2021-03-26 22:56:28 +0000

    Ability & Type added

--- js/scripts.js
+++ js/scripts.js
@@ -23,14 +23,14 @@ let pokemonRepository = (function () {
       delete content
       change content**/
       function addListItem(poke_mon){
-        let pokemonList = document.querySelector(".pokemonList"); // Created a varialbe | querySelector() returns the first element
-        let listpokemon = document.createElement("li"); // The document.createElement() accepts an HTML tag name and returns a new Node with the Element type.
+        let pokemonList = document.querySelector('.pokemonList'); // Created a varialbe | querySelector() returns the first element
+        let listpokemon = document.createElement('li'); // The document.createElement() accepts an HTML tag name and returns a new Node with the Element type.
         listpokemon.classList.add('list-group-item','col-2');
-        let button = document.createElement("button");
+        let button = document.createElement('button');
         button.innerText = poke_mon.name; //The innertext will add the pokemon on the button.
-        button.classList.add("text-capitalize","text-center" , "btn", "btn-primary");
-        button.setAttribute("data-toggle", "modal");
-        button.setAttribute("data-target", "#exampleModal");
+        button.classList.add('text-capitalize','text-center' , 'btn', 'btn-primary');
+        button.setAttribute('data-toggle', 'modal');
+        button.setAttribute('data-target', '#exampleModal');
         pokemonList.appendChild(listpokemon);
         listpokemon.appendChild(button); //The appendChild() method appends a node as the last child of a node.
 
@@ -39,13 +39,7 @@ let pokemonRepository = (function () {
         });
       }
 
-      function buttonListener(button, poke_mon) {
-        button.addEventListener('click', function () {
-          showDetails(poke_mon)
-        });
-      }
-
-      //This function job is collect all the pokemon from the array.
+        //This function job is collect all the pokemon from the array.
       function getAll() {
         return poke_mon;
       }
@@ -78,25 +72,26 @@ let pokemonRepository = (function () {
           item.weight = details.weight;
           item.height = details.height;
           item.types = details.types;
+          item.abilities = details.abilities;
         }).catch(function (e) {
           console.error(e);
         });
       }
 
   //---------------A function called showModal------------------------------
-let  modalContainer = document.querySelector("#exampleModal");
+let  modalContainer = document.querySelector('#exampleModal');
 
       function showModal(poke_mon) {
-        let modalBody = $(".modal-body"); //Created a variable: modalBody
-        let modalTitle = $(".modalTitle");//modalTitle
-        let modalHeader = $(".modal-header");//modalHeader and their function is to get the bootstrap tags in the index
-        // Clear all existing modal content. By not empty in it, it may add up everytime the model is open.
+        let modalBody = $('.modal-body'); //Created a variable: modalBody
+        let modalTitle = $('.modalTitle');//modalTitle
+        let modalHeader = $('.modal-header');//modalHeader and their function is to get the bootstrap tags in the index
+        // Clear all existing modal content. By not empty in it, itmay add up everytime the model is open.
         modalTitle.empty();
         modalBody.empty();
 
 //Modal body
 //-------------Creating element for name in the modal content------------------
-        let nameElement = $("<h1>" + poke_mon.name + "</h1>");// Created a variable: nameElement. This will display the name of the pokemon.
+        let nameElement = $('<h1>' + poke_mon.name + '</h1>');// Created a variable: nameElement. This will display the name of the pokemon.
 
 //--------------Creating an image in the modal content-------------------------
        let imgElement = document.createElement('img');
@@ -104,16 +99,22 @@ let  modalContainer = document.querySelector("#exampleModal");
        imgElement.src = poke_mon.imageUrl;
 
 //--------------Creating an element for the height-----------------------------
-        let heightElement = $("<p>" + "<strong>" + "Height : " + "</strong>" + poke_mon.height + "</p>");
+        let heightElement = $('<p>' + '<strong>' + 'Height : ' + '</strong>' + poke_mon.height + '</p>');
 
 //--------------Creating an element for the weight-----------------------------
-        let weightElement = $("<p>" + "<strong>" + "Weight : " + "</strong>" + poke_mon.weight + "</p>");
+        let weightElement = $('<p>' + '<strong>' + 'Weight : ' + '</strong>' + poke_mon.weight + '</p>');
 
 //--------------Creating an element for the type------------------------------
-        let typesElement = $("<p>" + "<strong>" + "Types : " + "</strong>" + poke_mon.types + "</p>");
+        let typesElement = '<p><strong>Types: </strong></p>'
+        poke_mon.types.forEach(function (el) {
+        typesElement = typesElement + '<p>' + el.type.name+ '</p>'
+       })
 
 //--------------Creating an element for abilites------------------------------
-        let abilitiesElement = $("<p>" + "<strong>" + "Abilities : " + "</strong>" + poke_mon.abilities + "</p>");
+       let abilitiesElement = '<p><strong>Abilities: </strong></p>'
+       poke_mon.abilities.forEach(function (el) {
+       abilitiesElement = abilitiesElement + '<p>' + el.ability.name+ '</p>'
+      })
 
 //----------------------Append--------------------------------------
 
